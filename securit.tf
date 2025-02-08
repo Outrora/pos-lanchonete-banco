2cae2a3e19d29bb31158c5d9d4b17a2b0cbd761c
Paulo Ferreira <mrpauloii@gmail.com>
2025-02-08 09:58:51 -0300

    Adicionado terraform

---
+++ securit.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group" "rds_sg" {
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Ajuste conforme necessário
+  }
+}
