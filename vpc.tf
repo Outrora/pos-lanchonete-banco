90dc049382749726cfc6da0e528432991a6beea4
Paulo Ferreira <mrpauloii@gmail.com>
2025-02-15 09:33:55 -0300

    :bricks: Correção na criação do banco de dados

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
   name   = "my-vpc"
   cidr   = "10.0.0.0/16"
 
-  azs                  = ["us-east-1a", "us-east-1b"]
+  azs                  = ["${var.regionDefault}a", "${var.regionDefault}b"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
