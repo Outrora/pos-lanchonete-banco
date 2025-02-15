3cf2c36f201e0f49a79a28d25253f93a040ccfa9
Paulo Ferreira <mrpauloii@gmail.com>
2025-02-15 09:33:55 -0300

    :bricks: Correção na criação do banco de dados

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
-resource "aws_db_instance" "rds" {
+resource "aws_db_instance" "pos-lanchonete" {
   identifier             = "my-postgres-db"
   engine                 = "postgres"
-  instance_class         = "db.t2.micro"
+  engine_version         = "16.3"
+  instance_class         = "db.t3.micro"
   allocated_storage      = 20
-  username               = "admin"
-  password               = "yourpassword" # Alterar para uma senha segura
-  parameter_group_name   = "default.postgres15"
+  username               = "paulo"
+  password               = "ferreira" # Alterar para uma senha segura
   publicly_accessible    = false
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+
+  # Para o nível gratuito, use armazenamento magnético
+  storage_type = "gp2"
 }
