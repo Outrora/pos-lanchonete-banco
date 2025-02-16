a26716cef14245249fd3b55114e216554a62d504
Paulo Ferreira <mrpauloii@gmail.com>
2025-02-16 11:52:28 -0300

    :construction_worker: Correção Variavel

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = var.regionDefault
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  token      = var.AWS_SESSION_TOKEN
+  //access_key = var.AWS_ACCESS_KEY_ID
+  //secret_key = var.AWS_SECRET_ACCESS_KEY
+  //token      = var.AWS_SESSION_TOKEN
 }
 
 
