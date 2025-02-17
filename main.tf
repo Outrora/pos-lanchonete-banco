99523d7181b6d33c488dda6258649ed770550bc8
Paulo Ferreira <mrpauloii@gmail.com>
2025-02-17 19:51:58 -0300

    :construction_worker: Adicionado Secrets

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "pos-lanchonete" {
   engine_version         = "16.3"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
-  username               = "paulo"
-  password               = "ferreira" # Alterar para uma senha segura
+  username               = "${var.user}"
+  password               = "${var.senha}" # Alterar para uma senha segura
   publicly_accessible    = false
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
--- vars.tf
+++ vars.tf
@@ -5,5 +5,8 @@ variable "regionDefault" {
 
 variable "TAGS" {}
 variable "NOME" {}
+
+variable "senha" {}
+variable "user" {}
 //variable "AWS_SECRET_ACCESS_KEY" {}
 //variable "AWS_SESSION_TOKEN" {}
