e57592cc7be0386a7ba08bc69dc5879a5e68fa00
Paulo Ferreira <mrpauloii@gmail.com>
2025-03-02 16:55:10 -0300

    :construction_worker:Adicionado Saida no SG

--- securit.tf
+++ securit.tf
@@ -6,6 +6,14 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Ajuste conforme necessário
+    cidr_blocks = ["0.0.0.0/0"] 
+    
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
