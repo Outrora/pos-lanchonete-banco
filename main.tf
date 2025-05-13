02aab23542db956eb12a883830c57a0c34a5ba5d
Paulo Ferreira <mrpauloii@gmail.com>
2025-05-13 08:48:09 -0300

    :construction_worker: Adicionado mais um banco de dados

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_db_instance" "pos-lanchonete" {
-  identifier             = "${var.NOME}-db"
+resource "aws_db_instance" "pos-lanchonete-cozinha" {
+  identifier             = "${var.NOME}-cozinha"
   engine                 = "postgres"
   engine_version         = "16.3"
   instance_class         = "db.t3.micro"
@@ -18,9 +18,37 @@ resource "aws_db_instance" "pos-lanchonete" {
     projeto = var.TAGS
   }
 
-  db_name = "${var.NOME}"
+  db_name = "${var.NOME}-cozinha"
+}
+
+resource "aws_db_instance" "pos-lanchonete-cliente" {
+  identifier             = "${var.NOME}-cliente"
+  engine                 = "postgres"
+  engine_version         = "16.3"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  username               = "${var.user}"
+  password               = "${var.senha}" # Alterar para uma senha segura
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+
+  # Para o nível gratuito, use armazenamento magnético
+  storage_type = "gp2"
+
+  tags = {
+    projeto = var.TAGS
+  }
+
+  db_name = "${var.NOME}-cliente"
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.pos-lanchonete.address
+  value = aws_db_instance.pos-lanchonete-cliente.address
+}
+
+
+output "rds_endpoint_cozinha" {
+  value = aws_db_instance.pos-lanchonete-cozinha.address
 }
