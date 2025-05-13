081be68741ddcb8d51f2dbcb6cc4c1e058f0b87b
Paulo Ferreira <mrpauloii@gmail.com>
2025-05-13 08:50:26 -0300

    :construction_worker: Corrigindo backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-pos-lanchonete"
+    bucket         = "terraform-pos-lanchonete-micros"
     key            = "bancoDados/state-file/terraform.tfstate"
     region         = "us-east-1"
   }
